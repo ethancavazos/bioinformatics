a4928974f43d1870a101cbcf9f9b00771248b662
ethancavazos <58490264+ethancavazos@users.noreply.github.com>
2021-10-29 08:34:07 -0500

    main

--- ML_DrugDiscovery.py
+++ ML_DrugDiscovery.py
@@ -1,3 +1,24 @@
 # importing libraries 
 import pandas as pd
-print('test')
+from chembl_webresource_client.new_client import new_client
+
+# search for target protein in Chembl database
+target = new_client.target
+target_query = target.search('coronavirus')
+targets = pd.DataFrame.from_dict(target_query)
+
+# display df
+# print(targets)
+
+# select / retrieve ID for bioactivity of target protein:
+# SARS cor. 3C-like proteinase
+selected_target = targets.target_chembl_id[4]
+# print(selected_target)
+
+# retrieve actual activity data
+activity = new_client.activity
+res = activity.filter(target_chembl_id=selected_target).filter(standard_type='IC50')
+
+df = pd.DataFrame.from_dict(res)
+print(df.head(10))
+print('hi')
